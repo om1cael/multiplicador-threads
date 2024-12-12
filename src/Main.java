6efcca89ca2a8590a6da7540c1e2d3dd25df1ad7
om1cael <188113439+om1cael@users.noreply.github.com>
2024-12-12 17:15:30 -0300

    comm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InterruptedException {
+        Mult mult = new Mult();
+
+        Thread thread = new Thread(() -> mult.multiplicar(5));
+        Thread thread2 = new Thread(() -> mult.multiplicar(5));
+        Thread thread3 = new Thread(() -> mult.multiplicar(5));
+        Thread thread4 = new Thread(() -> mult.multiplicar(5));
+
+        thread.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+    }
+}
+
+class Mult {
+    private int valor = 1;
+
+    public void multiplicar(int fator) {
+        this.valor *= fator;
+        this.getValor();
+    }
+
+    public void getValor() {
+        System.out.println(this.valor);
     }
 }
